81fc93de5151e80963b187ec8defccb772d2945e
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-06 14:30:13 -0600

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,20 +9,23 @@ export class Header extends React.Component {
     let categories = [{ name: 'Home', route: '/' }, { name: 'Stocks And Investing', route: '/stocks-investing' }, { name: 'About', route: '/about' }]
     return (
       <div className="">
-        <div className="banner-img d-flex align-items-center justify-content-end">
+        {/* <div className="banner-img d-flex align-items-center justify-content-end">
           <img src="https://i0.wp.com/moneywithcaleb.com/wp-content/uploads/2021/11/logo-2.png?w=493&ssl=1" className="logo-img"></img>
-        </div>
+        </div> */}
         <div className="container-fluid">
           <div className="row nav-background">
             <div className="col-12">
-              <div className="d-flex p-2">
-                {categories.map((c) => {
-                  return (
-                    <div>
-                      <Link to={c.route} className="category-text pe-4 text-dark">{c.name}</Link>
-                    </div>
-                  )
-                })}
+              <div className="d-flex p-2 align-items-end justify-content-between">
+                <h1 className="mb-0">Caleb Elliott</h1>
+                <div className="d-flex">
+                  {categories.map((c) => {
+                    return (
+                      <div>
+                        <Link to={c.route} className="category-text pe-5 text-dark">{c.name}</Link>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -16,7 +14,6 @@ root.render(
       <Route path="/" element={<App />} >
         <Route path="About" element={<About />} />
         <Route path="Home" element={<App />} />
-        <Route path="invoices" element={<Invoices />} />
         <Route path='Stocks-investing' element={<StocksInvesting />} />
       </Route>
     </Routes>
