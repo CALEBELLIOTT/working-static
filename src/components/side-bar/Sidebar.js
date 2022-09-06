968cca393d7abce305c726663a98015e9251df25
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-06 13:43:26 -0600

    side bar work

--- src/components/side-bar/Sidebar.js
+++ src/components/side-bar/Sidebar.js
@@ -5,10 +5,26 @@ export class Sidebar extends React.Component {
   render() {
     return (
       <div>
-        <img src='https://i.imgur.com/Kx2Mh8r.jpg' className="headshot mt-3"></img>
-        <h2 className="text-center mt-2 mb-0">Caleb Elliott</h2>
-        <p className="text-center text-success mb-3">Software Developer || Blogger</p>
-        <p>I'm glad you've found your way to my page! I am an experienced software developer </p>
+        <div>
+          <img src='https://i.imgur.com/Kx2Mh8r.jpg' className="headshot mt-3"></img>
+          <h2 className="text-center mt-2 mb-0">Caleb Elliott</h2>
+          <div className="d-flex justify-content-around my-2">
+            <a href="https://www.linkedin.com/in/caleb-elliott1/" target='_blank'>
+              <i className="mdi mdi-linkedin social-link linkedin"></i>
+            </a>
+            <a href="https://github.com/CALEBELLIOTT" target='_blank'>
+              <i className="mdi mdi-github social-link github"></i>
+            </a>
+            <a href="https://www.instagram.com/caleb.ellio/" target='_blank'>
+              <i className="mdi mdi-instagram social-link instagram"></i>
+            </a>
+          </div>
+          <p className="text-center text-success mb-3">Software Developer || Blogger</p>
+          <p className="intro-text">I am an experienced software developer who loves all things technology. This is my space to share my ideas about tech as well and maybe even a few random things that interest me! Enjoy your time here!</p>
+        </div>
+        <div>
+          <p></p>
+        </div>
       </div>
     )
   }
